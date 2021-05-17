97bfe4f40def96c206a268e857b6d8c2bfdad80b
Leonides <xxluckyleoxx1@gmail.com>
2021-05-17 16:49:11 -0400

    commit tree bd7273e3d55e26daae047ae3528677987827d75a

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,18 @@
 def unsafe?(speed)
-
+    if speed >= 40 && speed <= 60
+        resp = false
+        resp
+    else
+        resp = true
+        resp
+    end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? resp = true : resp = false
+    resp
 end
 	
 
